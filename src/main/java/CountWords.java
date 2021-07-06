5851f707a5c09418170afbc25e5b13d2948ba7ef
Gorokh Anton <rerearitet@gmail.com>
2021-07-06 22:42:14 +0300

    Initial commit

---
+++ src/main/java/CountWords.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Locale;
+
+public class CountWords {
+    public static String text = "Мама моет раму, раму мыла мама.";
+
+    public static void main(String[] args) {
+        int wordsRepeat;
+        int wordsNoRepeat;
+
+        String[] splitText = text.split(" ");
+        for (int i = 0; i < splitText.length; i++) {
+            splitText[i] = splitText[i].replaceAll("[^a-zA-ZА-яЁё]+", "").toUpperCase(Locale.ROOT);
+        }
+        wordsRepeat = splitText.length;
+
+        HashSet<String> splitTextSet = new HashSet<>(Arrays.asList(splitText));
+        splitText = splitTextSet.toArray(new String[0]);
+        wordsNoRepeat = splitText.length;
+
+        System.out.println(wordsRepeat);
+        System.out.println(wordsNoRepeat);
+    }
+}
